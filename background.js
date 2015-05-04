31c76db8a59ed7f5a6d970370800ced0d7c0a986
fkagami <kagami1990@gmail.com>
2015-05-04 22:37:44 +0900

    Cosmetic fix

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript(null,
     {file: "jquery.js"}, function(){
       chrome.tabs.executeScript(null,
-        {file: "sample1.js"}, function(){
+        {file: "geturl.js"}, function(){
         });
     });
 });
